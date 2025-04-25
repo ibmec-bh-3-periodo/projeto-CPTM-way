281ccb224c9c8f569f83c12f3aef65d34946c625
matheus <mtandradeg28@gmail.com>
2025-04-25 11:37:37 -0300

    Criando API de usuarios

--- src/db/server.js
+++ src/db/server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const fs = require("fs");
-const data =require("./users.json");
+const path = require("path");
 
 const server = express();
 server.use(express.json());
 
+// Caminho para o arquivo JSON
+const dataPath = path.join(__dirname, 'users.json');
+
+// Carregar dados iniciais (cria arquivo se não existir)
+let data = [];
+try {
+  data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    fs.writeFileSync(dataPath, '[]');
+  } else {
+    throw error;
+  }
+}
+
+server.listen(3333, () => {
+  console.log("Servidor ativo na porta 3333");
+});
+
 server.post("/cadastro", (req, res) => {
   const novoUsuario = req.body;
   data.push(novoUsuario);
-  fs.writeFileSync("./users.json", JSON.stringify(data, null, 2));
+  
+  // Escrever usando caminho absoluto
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+  
   res.status(201).send(data);
-});
-
-
-
-server.listen(3000, () => {
-  console.log("Servidor ativo na porta 3333");
 });
