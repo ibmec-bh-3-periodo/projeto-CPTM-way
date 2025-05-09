1364c3c43dd4b2c9064953af593b1108b3bc9fa6
matheus <mtandradeg28@gmail.com>
2025-05-09 10:37:57 -0300

    feat: fazendo cadastro

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,3 +1,10 @@
+const btnCadastrar = document.getElementById("button2");
+
+const email = document.getElementById("email");
+const confirmarEmail = document.getElementById("confirmarEmail");
+const senha = document.getElementById("senha");
+const confirmarSenha = document.getElementById("confirmarSenha");
+
 document.getElementById("cadastroForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
     window.location.href = "login.html"; 
